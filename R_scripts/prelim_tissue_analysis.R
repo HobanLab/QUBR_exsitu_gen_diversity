2c46ae635421c8066541a837a6905d2e2ec65440
Ash Hamilton <ahamilt@uchicago.edu>
2024-08-26 12:25:50 -0500

    adding code to make list of inds to extract

--- R_scripts/prelim_tissue_analysis.R
+++ R_scripts/prelim_tissue_analysis.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 
+`%notin%` <- Negate(`%in%`)
+
 field_data <- read_csv("./data/04_2024_field_datasheets_full.csv", na = c("N/A",""))
 
 #cleaning raw field data so we can graph it
@@ -41,42 +43,89 @@ tissue_data_clean <- tissue_data%>%
   rename(Weight = `Weight (g)`) %>% #rename weight column to remove units
   rename(QUBR_ID = `Accession ID`)%>%
   filter(str_detect(Color, "reen")) %>% #selects only colors named Green and green
-  filter(str_detect(Condition, "Good"))%>%
-  filter(Weight>=0.04) #removes leaf samples that are too small to do extraction with
-  #  
-
-tissue_data_clean%>%
-  ggplot() + #creates empty plot
-  geom_bar(aes(x = Color, fill = Color)) +
-  theme_classic() #removes gray background
-
-summary(tissue_field_data) #shows character vs numeric value details
+  filter(str_detect(Condition, "Good"))
 
 tissue_field_data <- tissue_data_clean%>% #joins field data and tissue transfer data by QUBR_ID
   left_join(., field_data_clean, join_by(QUBR_ID==QUBR_ID))
 
-tissue_field_data%>% #bar chart of number of tissue samples per region
+
+tissue_field_for_extraction <- tissue_field_data %>%
+  filter(Weight>=0.04) #removes leaf samples that are too small to do extraction with
+
+
+tissue_field_for_extraction%>%
+  ggplot() + #creates empty plot
+  geom_bar(aes(x = Color, fill = Color)) +
+  theme_classic() #removes gray background
+
+
+
+
+summary(tissue_field_for_extraction) #shows character vs numeric value details
+
+tissue_field_for_extraction%>% #bar chart of number of tissue samples per region
   ggplot() + 
   geom_bar(aes(x = Region, fill = Region)) +
   theme_classic()
 
-tissue_field_data%>% #box plot of tissue weights per region
+tissue_field_for_extraction%>% #box plot of tissue weights per region
   ggplot() +
   geom_boxplot(aes(x = Region, y = Weight, fill = Region)) +
   theme_classic()
 
-tissue_west <- tissue_field_data%>% #we will extract from all of these samples from the West
-  filter(Region == 'W')
-tissue_east <- tissue_field_data%>%
-  filter(Region == 'E')
-tissue_north <- tissue_field_data%>%
-  filter(Region == 'N')
-                        
-tissue_field_data %>%
+#how many inds from each region weigh enough to do extractions
+num_inds_by_region <- tissue_field_for_extraction %>%
   group_by(Region) %>%
   summarise(count = n())
+#v few from the W --> we are hoping to do 600 extractions which would mean 200 from each region and we have less than that from the W --> will do all of those and then divide the remaining number of inds by 2 to figure out how many inds to sample from E and N
 
-tissue_west%>%
+num_west <- num_inds_by_region$count[num_inds_by_region$Region=="W"][1]
+num_na <- num_inds_by_region$count[is.na(num_inds_by_region$Region)]
+
+num_to_sample <- floor((600-num_west-num_na)/2)
+
+#Separate tissue with high enough weight to do actual extractions into different regions
+tissue_west_and_NAs <- tissue_field_for_extraction%>% #we will extract from all of these samples from the West because there are so few
+  filter(Region == 'W' | is.na(Region))
+
+#look at the weights of the viable W inds to see how close to problematic they are
+tissue_west_and_NAs%>%
   ggplot() +
-  geom_col(aes(x = QUBR_ID, y = Weight))
+  geom_col(aes(x = QUBR_ID, y = Weight)) +
+  geom_hline(yintercept = .04, color = "red") + #add red line at minimum viable weight for extraction
+  theme_classic()
+#5 inds with literally no buffer room but mostly this seems fine
 
+tissue_east <- tissue_field_for_extraction%>%
+  filter(Region == 'E')
+
+tissue_north <- tissue_field_for_extraction%>%
+  filter(Region == 'N')
+                        
+
+#randomly sample the correct number of inds from N and E
+set.seed(2024)
+
+north_inds_to_sample <- sample(tissue_north$QUBR_ID, size = num_to_sample, replace = F)
+
+east_inds_to_sample <- sample(tissue_east$QUBR_ID, size = num_to_sample, replace = F)
+
+#create dataset with only 600 chosen inds for extractions
+tissue_field_600_inds <- tissue_field_for_extraction %>%
+  filter(QUBR_ID %in% tissue_west_and_NAs$QUBR_ID | QUBR_ID %in% north_inds_to_sample | QUBR_ID %in% east_inds_to_sample) 
+
+#take the top 100 heaviest weights, these will be the first 100 extractions 
+top_100_for_extractions <- tissue_field_600_inds %>%
+  top_n(., 100, wt = Weight) 
+
+#maybe unsurprisingly, nearly all of these are from the North
+top_100_for_extractions %>%
+  ggplot() +
+  geom_bar(aes(x = Region, fill = Region)) +
+  theme_classic()
+
+
+# Bind the remaining inds on to this list so we can keep these 100 as the first extractions but then proceed semi randomly
+top_100_for_extractions %>%
+  rbind(filter(tissue_field_600_inds, QUBR_ID %notin% top_100_for_extractions$QUBR_ID)) %>%
+  write_csv(., "./data/600_inds_for_extractions.csv")
