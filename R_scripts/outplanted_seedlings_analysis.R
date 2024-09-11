54db024f9bd2325802800e57c8b8e1fa61092a5b
danabarry23 <danabarry23@gmail.com>
2024-09-11 15:58:53 -0500

    Update outplanted_seedlings_analysis.R
    Adding 2024 datasheet to existing 2023 code, connecting using row_bind, recalculating TimeAlive

--- R_scripts/outplanted_seedlings_analysis.R
+++ R_scripts/outplanted_seedlings_analysis.R
@@ -5,16 +5,23 @@ library(tibble)
 library(lubridate)
 setwd("..")
 
+#assigns values for the dates monitoring took place
+Today <- today()
+Monitor1Date <- dmy("13/02/2022")
+Monitor2Date <- dmy("20/01/2023")
+Monitor3Date <- dmy("13/12/2023")
 
-#imports one of Daniel's databases
-outplanted_seedlings <- read_csv("./data/Datos de Siembra en Ranchos_Actualizado_05_2024.xlsx - Datos_Campaña_Seimbra_2023.csv")
+#imports Daniel's databases (2023 and 2024 tabs)
+outplanted_seedlings23 <- read_csv("./data/Datos de Siembra en Ranchos_Actualizado_05_2024.xlsx - Datos_Campaña_Seimbra_2023.csv")
+outplanted_seedlings24 <- read_csv("./data/Datos de Siembra en Ranchos_Actualizado_05_2024.xlsx - Datos_Campaña_Seimbra_2024.csv")
+summary(outplanted_seedlings23)
+summary(outplanted_seedlings24)
 
-summary(outplanted_seedlings)
-
-#uses outplanted_seedlings dataset to create a cleaned dataset
-seedlings_clean <- outplanted_seedlings%>% 
-
+#combines data from 2023 and 2024 tabs
+seedlings_combined <- bind_rows(outplanted_seedlings23, outplanted_seedlings24)
+summary(seedlings_combined)
 #renames columns to simplified English, differentiates seed origin and seedling planted region
+seedlings_clean <- seedlings_combined%>%
   rename(Name = 'Nombre')%>%
   rename(Town = 'Localidad')%>%
   rename(Ranch = 'Rancho, Lugar o Sitio')%>%
@@ -25,60 +32,52 @@ seedlings_clean <- outplanted_seedlings%>%
   rename(PlantedReg = 'Región')%>%
   rename(DatePlanted = 'Fecha transplante')%>%
   rename(YearCollected = 'Año de colecta')%>%
-
-#reclass Perdida (poor) as Muerta (dead)
-  mutate(Monitor1=recode(Monitor1, 'Perdida' = 'Muerta'))%>%
-
+  rename(Observations = 'Observaciones')%>% 
+  rename(Contact = 'Contacto')%>%
+  rename(PlantedIn = 'Sembrado en:')%>%
+  rename(Watered = 'Lluvia solida')%>%
+  rename(Monitor4 = 'Monitoreo 1 (__/__/__)')%>%
+  rename(OriginLabelAsh = 'Procedencia Etiqueta Ash Abril 2024')%>%
+  mutate(Monitor1=recode(Monitor1, 'Perdida' = 'Muerta'))%>%#reclass Perdida (poor) as Muerta (dead)
 #calculate when a seedling died based on the last Monitoring date it was seen alive
   add_column(DateDied = NA)%>%
-  mutate(DateDied = case_when(Monitor1 == 'Muerta' ~ DatePlanted,
-                              Monitor1 == 'Viva' & Monitor2 == 'Muerta' ~ DatePlanted,
-                              Monitor1 == 'Nueva' & Monitor2 == 'Muerta' ~ '13-02-2022',
-                              Monitor1 == 'Nueva' & Monitor3 == 'Muerta' ~ '20-01-2023',
-                              Monitor1 == 'Viva' & Monitor3 == 'Muerta' ~ '20-01-2023',
-                              Monitor2 == 'Nueva' & Monitor3 == 'Muerta' ~ '20-01-2023'))%>%
-
+  mutate(DateDied = case_when((Monitor1 == 'Muerta') | (Monitor1 == 'Viva' & Monitor2 == 'Muerta') ~ DatePlanted,
+                              (Monitor1 == 'Nueva' & Monitor2 == 'Muerta') ~ '13-02-2022',
+                              (Monitor1 == 'Nueva' & Monitor3 == 'Muerta') | (Monitor1 == 'Viva' & Monitor3 == 'Muerta') | (Monitor2 == 'Nueva' & Monitor3 == 'Muerta') ~ '20-01-2023'))%>%
 #format date as DayMonthYear
+  add_column(TimeAlive = NA)%>%
+  add_column(RatioTimeAlive = NA)%>%
+  add_column(PotentialTimeAlive = NA)%>%
+  
   mutate(DateDied = dmy(DateDied))%>%
   mutate(DatePlanted = dmy(DatePlanted))%>%
-  add_column(MonthsAlive = NA)%>%
-
-#calculate MonthsAlive as difference between DatePlanted and DateDied
-  mutate(MonthsAlive = DateDied - DatePlanted)
-
-#assigns values for the dates monitoring took place
-Monitor1Date <- dmy("13/02/2022")
-Monitor2Date <- dmy("20/01/2023")
-Monitor3Date <- dmy("13/12/2023")
+  mutate(RatioTimeAlive = dmy(RatioTimeAlive))%>%
+  mutate(PotentialTimeAlive = dmy(PotentialTimeAlive))%>%
+  
+#calculate TimeAlive as difference between DatePlanted and DateDied
+  mutate(TimeAlive = DateDied - DatePlanted)%>%
+  mutate(PotentialTimeAlive = Today - DatePlanted)%>%#days since it was first planted
+  mutate(RatioTimeAlive = TimeAlive / PotentialTimeAlive) #this line doesn't work because you can't divide difftimes, but I needed to convert those columns to  
+  
 
 summary(seedlings_clean)
-unique(seedlings_clean$DatePlanted)
 ?duration
+?class
+
 
 #separates seedlings into seed origin region
-seedlings_E <- seedlings_clean%>%
+seedlings_E <- seedlings_clean23%>%
   filter(OriginReg == 'Golfo')
-seedlings_W <- seedlings_clean%>%
+seedlings_W <- seedlings_clean23%>%
   filter(OriginReg =='Pacifico')
-seedlings_N <- seedlings_clean%>%
+seedlings_N <- seedlings_clean23%>%
   filter(OriginReg == 'Norte')
-
 #includes all seedlings that were dead at 1st OR 2nd monitoring
-seedlings_dead <- seedlings_clean%>% 
+seedlings_dead <- seedlings_clean23%>% 
   filter(Monitor1 == "Muerta" | Monitor2 == "Muerta")
-
 #includes all seedlings that were alive as of 2nd monitoring
-seedlings_alive <- seedlings_clean%>%
+seedlings_alive <- seedlings_clean23%>%
   filter(str_detect(Monitor2, "Viva"))
-
-#there are more town names in Daniel's database than we used previously         
-seedlings_clean %>%
-  group_by(Town) %>%
-  summarise(count = n())
-
-
-
-
 #survival of seedlings by town
 #EAST 
 seedlings_E%>% 
@@ -98,23 +97,8 @@ seedlings_N%>%
   geom_bar(aes(x = Monitor2, fill = Monitor2)) +
   facet_grid(~Town) +
   theme_classic()
-
-
-
 #seedling outcome by region
-seedlings_clean%>%
-  ggplot() +
-  geom_bar(aes(x = Monitor2, fill = Monitor2)) +
-  facet_grid(~OriginReg) +
-  theme_classic()
-
-seedlings_clean%>%
-  ggplot() +
-  geom_bar(aes(x = Monitor2, fill = Monitor2)) +
-  facet_grid(~OriginReg) +
-  theme_classic()
-
-seedlings_clean%>%
+seedlings_clean23%>%
   ggplot() +
   geom_bar(aes(x = Monitor2, fill = Monitor2)) +
   facet_grid(~OriginReg) +
@@ -122,4 +106,10 @@ seedlings_clean%>%
 
 unique(seedlings_W$Monitor2)
 
+#there are more town names in Daniel's database than we used previously         
+seedlings_clean23 %>%
+  group_by(Town) %>%
+  summarise(count = n())
+
 #what columns are only in one tab, add those to the other tab, rbind command to combine (2024 to bottom of)
+?rbind
